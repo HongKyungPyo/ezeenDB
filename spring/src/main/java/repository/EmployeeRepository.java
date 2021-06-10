bf8b27a4c11cdc51ab6d797a356258aa7b35ee30
EZEN <EZEN@DESKTOP-ORND7UU>
2021-06-10 19:44:24 +0900

    20210610

--- spring/src/main/java/repository/EmployeeRepository.java
+++ spring/src/main/java/repository/EmployeeRepository.java
@@ -3,8 +3,17 @@ package repository;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import model.EmployeeDTO;
+
 public class EmployeeRepository {
 	@Autowired
 	SqlSession sqlsession;
+	private final String namespace="mappers.employeeMapper";
+	private String statement;
+	public void insertEmployee(EmployeeDTO dto)
+	{
+		statement= namespace + ".employeeInsert";
+		sqlsession.insert(statement, dto);
+	}
 	
 }
--- spring/src/main/java/service/employees/EmployeeService.java
+++ spring/src/main/java/service/employees/EmployeeService.java
@@ -1,13 +1,20 @@
 package service.employees;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import command.EmployeeCommand;
 import model.EmployeeDTO;
+import repository.EmployeeRepository;
 
 public class EmployeeService {
-
+	@Autowired
+	BCryptPasswordEncoder bcryptPasswordEncoder;
+	@Autowired
+	EmployeeRepository employeeRepository;
 	public void insertEmp(EmployeeCommand employeecommand)
-	{
-		
+	{	
+				
 			EmployeeDTO dto= new EmployeeDTO();
 			dto.setEmpDeptNumber(employeecommand.getEmpDeptNumber());
 			dto.setEmpEmail(employeecommand.getEmpEmail());
@@ -15,9 +22,12 @@ public class EmployeeService {
 			dto.setEmpName(employeecommand.getEmpName());
 			dto.setEmpNo(employeecommand.getEmpNo());
 			dto.setEmpPhoneNumber(employeecommand.getEmpPhoneNumber());
-			dto.setEmpPw(employeecommand.getEmpPw());
+			dto.setEmpPw(bcryptPasswordEncoder.encode(
+					employeecommand.getEmpPw()));
 			dto.setHireDate(employeecommand.getHireDate());
 			dto.setSalary(employeecommand.getSalary());
+			employeeRepository.insertEmployee(dto);
+			
 		
 	}
 }
